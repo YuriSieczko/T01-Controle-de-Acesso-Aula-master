df92d2b647ecefc86858538726d3cacb401ab718
Joana Cabral <joanacabralmartins@outlook.com>
2023-09-17 23:54:31 -0300

    controllers e rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\NoticiaController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,5 +25,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 Route::resource('/noticias', NoticiaController::class);
+Route::resource('/roles', RoleController::class);
+Route::resource('/permissions', PermissionController::class);
+Route::resource('/users', UserController::class);
 
 require __DIR__.'/auth.php';
